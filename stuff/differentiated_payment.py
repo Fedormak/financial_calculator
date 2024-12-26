2e204295dc153b01be147ce23cd1491bf2cae805
Makbich <fedmakru@gmail.com>
2024-12-26 17:32:15 +0300

    version 3

---
+++ stuff/differentiated_payment.py
@@ -0,0 +1,11 @@
+
+class annuity_payment:
+
+    def payment(self, sumCredit=None, month=None, percent=None ):
+        percent = percent / 12
+        percent = percent / 100
+        kof = (percent * ((1 + percent) ** month)) / (((1 + percent) ** month) - 1)
+        payment = sumCredit * kof
+        return round(payment, 2)
+
+
